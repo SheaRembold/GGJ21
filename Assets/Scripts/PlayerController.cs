598b51a5a11e0102fa3d63e61b3f25f843e82874
shea <siarck@gmail.com>
2021-01-29 20:38:39 -0500

    initial project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 10;
+
+    public static PlayerController Instance;
+
+    Rigidbody2D rigidbody;
+    InputActionAsset actions;
+
+    void Awake()
+    {
+        Instance = this;
+
+        rigidbody = GetComponent<Rigidbody2D>();
+        actions = GetComponent<PlayerInput>().actions;
+    }
+
+    void Update()
+    {
+        rigidbody.velocity = actions.FindAction("Move").ReadValue<Vector2>() * moveSpeed;
+    }
+}
